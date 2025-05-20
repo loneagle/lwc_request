e4c1fd11f4431374c4acb66f3a0cc5e212f50a1c
Dmytro Demchuk <>
2025-05-20 10:39:50 +0300

    Create tests update shippingitem metadata

--- src/lwc/shippingItem/shippingItem.js
+++ src/lwc/shippingItem/shippingItem.js
@@ -1,21 +1,32 @@
-import {LightningElement} from 'lwc';
-import getStatus from '@salesforce/apex/ShippingItemController.getStatus';
+import {LightningElement, api} from 'lwc';
+import getOrderShipmentStatus from '@salesforce/apex/ShippingItemController.getOrderShipmentStatus';
 
 export default class ShippingItem extends LightningElement {
+	@api recordId;
 	shipmentStatus;
 	spinnerEnabled = true;
 
 	connectedCallback() {
 		this.updateStatus();
 	}
-	
+
 	updateStatus() {
-		getStatus().then(result => {
-			this.shipmentStatus = result;
-		}).catch(error => {
-			console.log(error);
-		}).finally(() => {
+		if (!this.recordId) {
+			this.shipmentStatus = 'Error - No Order Id provided';
 			this.spinnerEnabled = false;
-		})
+			return;
+		}
+
+		getOrderShipmentStatus({ orderId: this.recordId })
+			.then(result => {
+				this.shipmentStatus = result;
+			})
+			.catch(error => {
+				console.error('Error retrieving shipment status:', error);
+				this.shipmentStatus = 'Error retrieving shipment status';
+			})
+			.finally(() => {
+				this.spinnerEnabled = false;
+			});
 	}
 }
